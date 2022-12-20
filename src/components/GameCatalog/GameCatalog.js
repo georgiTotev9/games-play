e872344cc5e7c724caeb313d5f41521eea4a6a0d
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2022-12-20 17:18:51 +0200

    catalog final changes

--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -4,21 +4,29 @@ import GameCard from './GameCard';
 
 const GameCatalog = () => {
     const [games, setGames] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+        setLoading(true);
+        
         fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
             .then((response) => response.json())
-            .then((games) => setGames(games));
+            .then((games) => {
+                setGames(games);
+                setLoading(false);
+            });
     }, []);
 
     return (
         <section id='catalog-page'>
             <h1>All Games</h1>
+            {loading && <h3 className='no-articles'> Loading...</h3>}
 
-            {games.map((game, index) => (
-                <GameCard key={index} game={game} />
-            ))}
-
-            <h3 className='no-articles'>No articles yet</h3>
+            {games.length > 0 ? (
+                games.map((game) => <GameCard key={game._id} game={game} />)
+            ) : (
+                <h3 className='no-articles'>No articles yet</h3>
+            )}
         </section>
     );
 };
